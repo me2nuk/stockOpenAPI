6c554b36301eb8f71fad217f868e30e7404d114b
kimtong_code <57348147+kimminwyk@users.noreply.github.com>
2021-01-14 13:55:26 +0900

    add
    add

--- TelegramBot/__setting__.py
+++ TelegramBot/__setting__.py
@@ -1,3 +1,4 @@
-__BotVersion__ = "Telegram-Bot_1.0"
+__BotVersion__ = "Telegram-Bot_1.1"
 __BoTelegramName__ = "PrediCtionMoneyBot"
 __BotTelegramToken__ = "1408660707:AAFD5kKSUtIdQ_T5ZEGV9iERM-88YWbeWzI"
+#BotTelegramToken variable in You Telegram Token Input plese
