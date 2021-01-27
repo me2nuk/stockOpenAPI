14ce026435e37e5d4184131459d2cfc9ab7d5b23
kimminwyk <57348147+kimminwyk@users.noreply.github.com>
2021-01-27 21:10:39 +0900

    Telegram Module Remove
    Telegram Module -> https://github.com/kimminwyk/TelegramBot

--- TelegramBot/__init__.py
+++
@@ -1,6 +0,0 @@
-#Telegram Bot __init__ Type python File
-from Telegram_CommandList import *
-from __setting__ import *
-from ClassRaise import *
-from BotStart import *
-from __help__ import *
--- TelegramBot/__setting__.py
+++
@@ -1,4 +0,0 @@
-__BotVersion__ = "Telegram-Bot_1.1"
-__BoTelegramName__ = "PrediCtionMoneyBot"
-__BotTelegramToken__ = "1408660707:AAFD5kKSUtIdQ_T5ZEGV9iERM-88YWbeWzI"
-#BotTelegramToken variable in You Telegram Token Input plese
