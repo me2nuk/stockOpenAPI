09f49555701a84c89e7c563193857befbc33b454
kimminwyk <57348147+kimminwyk@users.noreply.github.com>
2021-02-07 21:48:15 +0900

    Updaet README.md

--- test/python/CLI/kiwoom_OnEventConnect.py
+++ test/python/CLI/kiwoom_OnEventConnect.py
@@ -5,8 +5,13 @@ import sys
 class PyQt_kiwoomConnect:
     def __init__(self) -> None:
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
-        self.kiwoom.dynamicCall("CommConnect()")
-        print('kiwoom OpenAPI CommConnect() 호출 성공')
+        self.kiwoom.dynamicCall("CommConnect()")    
+        #self.kiwoom.OnEventConnect.connect(self.Login_Event)
+        self.kiwoom.OnEventConnect.connect(self.Login_Event)
+    def Login_Event(self, err_code):
+        if err_code == 0:
+            print('키움증권 OpenAPI+ 로그인 성공')
+            sys.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
